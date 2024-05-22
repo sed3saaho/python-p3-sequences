1344be05113c3d8273a457068fb38437ce70d979
Sedly <sedly.keith@student.moringaschool.com>
2024-05-22 12:54:53 +0300

    pthon-p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    '''function print_fibonacci()'''
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fib = [0, 1]
+        for i in range(2, length):
+            fib.append(fib[i - 1] + fib[i - 2])
+        print(fib)
